51c8af993f6819f716089760a90f01f1ca0fbcef
LatrachDev <m.latrach.youcode@gmail.com>
2025-05-21 15:01:55 +0100

    ES-1 Welcome page design improve styling

--- src/screens/WelcomeScreen/WelcomeScreen.styles.ts
+++ src/screens/WelcomeScreen/WelcomeScreen.styles.ts
@@ -5,13 +5,25 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#EBF4F6',
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        paddingHorizontal: 20, // for slight horizontal spacing
     },
     logo: {
-        width: 200,
-        height: 200,
+        width: 300,
+        height: 300,
         resizeMode: 'contain',
+        marginBottom: 40, // space between logo and button
     },
+    button: {
+        backgroundColor: '#58BECA',
+        marginTop: '30%',
+        width: '70%',
+        padding: 5,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        alignItems: 'center',
+        borderRadius: 10,
+    }
 })
 
-export default styles;
+export default styles
--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -4,9 +4,16 @@ import styles from './WelcomeScreen.styles';
 
 const WelcomeScreen: React.FC = () => {
     return (
-        <View style={styles.container}>
-            <Image source={require('../../assets/images/Escodeve-logo.png')} />
-        </View>
+        <>
+            <View style={styles.container}>
+                <Image style={styles.logo} source={require('../../assets/images/Escodeve-logo.png')} />
+            
+                <View style={styles.button}>
+                    <Image source={require('../../assets/images/arrow_forward.png')} />
+                </View>
+            
+            </View>
+        </>
     );
 };
 
